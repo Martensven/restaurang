39779c5afddc3d00feff0414cda3c3e58a673cbb
Martensven <martengullstrom@gmail.com>
2024-12-16 13:17:42 +0100

    Added import/export

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const db = {
   "bbqs": [
     {
       "id": "ribs-brisket-and-burnt-ends",
-      "img": "https://goldbelly.imgix.net/uploads/showcase_media_asset/image/79619/joes-kc-ribs-brisket-and-burnt-ends.6710e994980e485e6441b794717ad6fb.jpg?ixlib=react-9.0.2&auto=format&ar=1%3A1",
+      img: "https://goldbelly.imgix.net/uploads/showcase_media_asset/image/79619/joes-kc-ribs-brisket-and-burnt-ends.6710e994980e485e6441b794717ad6fb.jpg?ixlib=react-9.0.2&auto=format&ar=1%3A1",
       "name": "Joe's KC BBQ",
       "dsc": "Joe's KC Ribs, Brisket & Burnt Ends",
       "price": 110.99,
@@ -13145,4 +13145,5 @@ const db = {
   }
 }
 
-console.log(db["best-foods"]);
+
+export default db;
--- script.js
+++ script.js
@@ -1,4 +1,20 @@
+import db from '/db.js';
 
+console.log(db.bbqs);
 
+const dishes = db["bbqs"]; // Ändra till korrekt nyckel
+const menu = document.getElementById("menu"); // Kontrollera att elementet finns
 
+Object.values(dishes).forEach(dish => {
+    const section = document.createElement("section"); // Skapa ett element för varje rätt
+    section.className = "dish"; // Varje section får ett klassnamn, detta kan vi använda för styling
 
+    section.innerHTML = `
+        <img src="${dish.img}" alt="${dish.name}">
+        <h2>${dish.name}</h2>
+        <p>${dish.dsc}</p>
+        <p>Pris: ${dish.price}:-</p>
+    `; // Allt efter '=' är vad vi vill att våra sektioner ska fyllas med
+
+    menu.appendChild(section); // Vi använder append för att sedan lägga till section-elementen.
+});
